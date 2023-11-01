72ed44f58daa63bb207b20acee61de671d0ea461
Ajmal-Ali-k <ajmalali749@gmail.com>
2023-11-01 14:05:28 +0530

    design updates

--- src/Api/api.jsx
+++ src/Api/api.jsx
@@ -1,2 +1,2 @@
-//  export default 'https://doccure.onrender.com';
-export default 'http://localhost:8080';
+ export default 'https://doccure.onrender.com';
+// export default 'http://localhost:8080';
