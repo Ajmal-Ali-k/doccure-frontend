2ba8b36877dcd39a438b7b35b59dd848d76d325a
Ajmal-Ali-k <ajmalali749@gmail.com>
2023-11-01 14:02:03 +0530

    some updatesin design

--- src/Context/SocketProvider.js
+++ src/Context/SocketProvider.js
@@ -9,12 +9,19 @@ export const useSocket =()=>{
 
 }
 
+// export const SocketProvider = (props) =>{
+//     const socket = useMemo(()=> io('https://doccure.onrender.com'),[])
+//     return (
+//         <SocketContext.Provider value={socket}>
+//             {props.children}
+//         </SocketContext.Provider>
+//     )
+// }
 export const SocketProvider = (props) =>{
-    const socket = useMemo(()=> io('https://doccure.onrender.com'),[])
+    const socket = useMemo(()=> io('http://localhost:8080'),[])
     return (
         <SocketContext.Provider value={socket}>
             {props.children}
         </SocketContext.Provider>
     )
 }
- 
